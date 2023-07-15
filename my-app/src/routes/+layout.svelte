7fd4c11bb5b6db3278409a3fffcaa0efa984e3d8
Matty <smithmatthewc@protonmail.com>
2023-07-15 13:08:31 -0500

    added basic styles

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import '@picocss/pico'
+</script>
+
+<div class="container">
+    <hgroup>
+        <h1>
+            <a href="/">home</a>
+        </h1>
+        <h2>
+            let's do it
+        </h2>
+    </hgroup>
+    <slot />
+</div>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="grid">
+    <div>
+        <h2>things:</h2>
+        <article>
+            <header>examplo</header>
+            <p>
+                blah bla
+            </p>
+            <button class="outline secondary">deletos</button>
+            <a href="/" role="button" class="outline contrast" style="width: 100%;">edit</a>
+        </article>
+    </div>
+    <form action="" method="post">
+        <h3>new thing</h3>
+        <label for="title">title</label>
+        <input type="text" id="title" name="title">
+        <label for="title">title</label>
+        <textarea name="content" id="content" rows={5}></textarea>
+        <button type="submit">add thing</button>
+    </form>
+</div>
